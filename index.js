ec57f1392912f70ead310f55f8e3b62485499425
michaelippolito <70740198+michaelippolito826@users.noreply.github.com>
2022-02-17 16:04:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const fillGrid = () => {
+    let str_input = "pwwkew";
+    let visual = document.getElementsByClassName('visual')[0];
+
+    //DOM Manipulation for string and pointers
+    for (let index = 0; index < str_input.length; index++) {
+        let char_div = document.createElement('div');
+        char_div.className = 'visual_content'
+        char_div.innerHTML = str_input[index];
+        visual.appendChild(char_div);
+    }
+    visual.style.gridTemplateColumns = `repeat(${str_input.length}, 1fr)`;
+    let start_pointer = document.getElementsByClassName('start_pointer')[0];
+    start_pointer.style.gridColumn = "2";
+
+    //algo implementation
+    let set = new Set();
+    let count = 0;
+    let start_pointer_value = 0;
+    //loop through string using sliding window technique
+    for (let right_pointer_value in str_input) {
+        while (set.has(str_input[right_pointer_value])) {
+            set.delete(str_input[start_pointer_value]);
+            start_pointer_value++;
+        }
+    set.add(str_input[right_pointer_value]);
+    count = Math.max(count, right_pointer_value - start_pointer_value + 1);
+    }
+    console.log(count);
+}
+
+fillGrid();
